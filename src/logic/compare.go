0eb7286a4820844330fe81057c775d129e554682
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-10-23 18:19:02 +0300

    Separate test file variants

--- src/logic/compare.go
+++ src/logic/compare.go
@@ -23,54 +23,48 @@
 package logic
 
 import (
-	"fmt"
 	"strings"
 )
+
+func TestFileVariants(path string) []string {
+	splittedSrcFile := strings.Split(path, "/")
+	var srcFileRelative string
+	if len(splittedSrcFile) == 1 {
+		srcFileRelative = path
+	} else {
+		srcFileRelative = strings.Join(splittedSrcFile[1:], "/")
+	}
+	fileExtension := "." + strings.Split(srcFileRelative, ".")[1]
+	splittedPath := strings.Split(srcFileRelative, "/")
+	fileName := splittedPath[len(splittedPath)-1]
+	fileNameWithoutExtension := strings.Split(fileName, ".")[0]
+	return []string{
+		strings.Replace(
+			srcFileRelative,
+			fileName,
+			fileNameWithoutExtension+"_test"+fileExtension,
+			1,
+		),
+		strings.Replace(
+			srcFileRelative,
+			fileName,
+			"test_"+fileNameWithoutExtension+fileExtension,
+			1,
+		),
+	}
+}
 
 func Compare(srcDir Directory, testsDir Directory) []string {
 	filesWithoutTests := make([]string, 0)
 	testFiles, _ := testsDir.Structure()
 	srcFiles, _ := srcDir.Structure()
 	for _, srcFile := range srcFiles {
-		// absolutePath, _ := srcFile.Absolute()
 		relativePath, _ := srcFile.Relative()
-		// fmt.Printf("src absolute: %s src relative: %s\n", absolutePath, relativePath)
-		splittedSrcFile := strings.Split(relativePath, "/")
-		var srcFileRelative string
-		if len(splittedSrcFile) == 1 {
-			srcFileRelative = relativePath
-		} else {
-			srcFileRelative = strings.Join(splittedSrcFile[1:], "/")
-		}
-		// fmt.Printf("src relative: %s splitted: %v\n", srcFileRelative, strings.Split(srcFileRelative, "."))
-		fmt.Printf("src relative: %s\n", srcFileRelative)
-		// os.Exit(0)
-		fileExtension := "." + strings.Split(srcFileRelative, ".")[1]
-		splittedPath := strings.Split(srcFileRelative, "/")
-		fileName := splittedPath[len(splittedPath)-1]
-		fileNameWithoutExtension := strings.Split(fileName, ".")[0]
-		testFileVariants := []string{
-			strings.Replace(
-				srcFileRelative,
-				fileName,
-				fileNameWithoutExtension+"_test"+fileExtension,
-				1,
-			),
-			strings.Replace(
-				srcFileRelative,
-				fileName,
-				"test_"+fileNameWithoutExtension+fileExtension,
-				1,
-			),
-		}
+		testFileVariants := TestFileVariants(relativePath)
 		testFileFound := false
-		fmt.Printf("test variants: %v\n", testFileVariants)
 	out:
 		for _, testFile := range testFiles {
-			// absolutePath, _ := testFile.Absolute()
 			relativePath, _ := testFile.Relative()
-			// fmt.Printf("src absolute: %s src relative: %s\n", absolutePath, relativePath)
-			// splittedTestFile := strings.Split(absolutePath, "/")
 			splittedTestFile := strings.Split(relativePath, "/")
 			testFileRelative := strings.Join(splittedTestFile[1:], "/")
 			for _, testFileVariant := range testFileVariants {
